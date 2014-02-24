c4d57bf6bbfcc7fc70756f398d055285e7eb56f2
vlad fatu <colegu123@gmail.com>
2014-02-24 18:42:33 +0200

    - started working on matrix probabilities generator

--- src/com/airtactics/aitester/utils/MatrixProbabilitiesGenerator.java
+++ src/com/airtactics/aitester/utils/MatrixProbabilitiesGenerator.java
@@ -1,5 +1,47 @@
 package com.airtactics.aitester.utils;
 
-public class MatrixProbabilitiesGenerator {
+import com.airtactics.aitester.coreitems.Point;
 
+public class MatrixProbabilitiesGenerator {
+	
+	public final static int GRID_SIZE = 10;
+	
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	public boolean isHeadValid(Point head, int degrees)
+	{
+		if (degrees == 0)
+		{
+			if (head.x > 0  && head.x < GRID_SIZE-1 && head.y >= 0 && head.y < GRID_SIZE - 3)
+			{
+				return true;
+			}
+		}
+		else if (degrees == 90)
+		{
+			if (head.x > 2  && head.x < GRID_SIZE && head.y > 0 && head.y < GRID_SIZE - 1)
+			{
+				return true;
+			}
+		}
+		else if (degrees == 180)
+		{
+			if (head.x > 0  && head.x < GRID_SIZE-1 && head.y > 2 && head.y < GRID_SIZE)
+			{
+				return true;
+			}
+		}
+		else if (degrees == 270)
+		{
+			if (head.x >= 0  && head.x < GRID_SIZE-3 && head.y > 0 && head.y < GRID_SIZE - 1)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
 }
